b2a46681b26e8143d75f20b4d4b71ab6bdb20886
Nat Epstein <nathaniel.epstein@gmail.com>
2022-01-06 16:17:59 -0500

    added starter code for controllers

---
+++ controllers/thought-controller.js
@@ -0,0 +1,68 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+  // get all thoughts
+  getThoughts(req, res) {
+    // TODO: Your code here
+
+  },
+  
+  // get single thought by id
+  getSingleThought(req, res) {
+    // TODO: Your code
+
+  },
+
+  // create a thought
+  createThought(req, res) {
+    // TODO: create a thought and add the thought to user's thoughts array
+
+  },
+
+  // update thought
+  updateThought(req, res) {
+    // TODO: update thought
+
+  },
+
+  // delete thought
+  deleteThought(req, res) {
+    Thought.findOneAndRemove({ _id: req.params.thoughtId })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: 'No thought with this id!' });
+        }
+
+        // remove thought id from user's `thoughts` field
+        return User.findOneAndUpdate(
+          { thoughts: req.params.thoughtId },
+          { $pull: { thoughts: req.params.thoughtId } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'Thought created but no user with this id!' });
+        }
+        res.json({ message: 'Thought successfully deleted!' });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // add a reaction to a thought
+  addReaction(req, res) {
+    //  TODO: add reaction to thought's reaction array
+
+  },
+
+  // remove reaction from a thought
+  removeReaction(req, res) {
+    // TODO: remove reaction from thoughts
+
+  },
+};
+
+module.exports = thoughtController;
